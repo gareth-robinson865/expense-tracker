370eaebae279280535d9d293764e2de9f93583e1
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-04-27 11:17:21 +0100

    function created and event added to prevent page from  reloading when the button is pressed

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+const createPayment = (ev) => {
+    //prevent the page from reloading when the button is pressed
+    ev.preventDefault();
+}
+
 let person = document.getElementById("who-from").value;
 let workCompleted = document.getElementById("what-for").value;
+
+//each created payment needs to have a reference
