f6689059a3ea00d91af4454ec57c922941282af5
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-04-28 15:46:02 +0100

    variable created for the radio buttons

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const createPayment = (ev) => {
     //prevent the page from reloading when the button is pressed
     ev.preventDefault();
 
+    //variable for the radio buttons
+    let incomeOrExpenditure = document.forms[0];
+
+    //for loop to check which radio button is selected
+
     //need to create control flow here for which radio button is highlighted
     
 }
