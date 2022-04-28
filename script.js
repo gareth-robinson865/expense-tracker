c63bf2f03227a748fd8e697f0472d87a34586290
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-04-28 16:59:51 +0100

    function updated to get the title from the form

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ const createPayment = (ev) => {
     for(i=0; i < incomeOrExpenditure.length; i++) {
         if(incomeOrExpenditure[i].checked) {
             let value = incomeOrExpenditure[i].value;
-            console.log(value);
-            console.log(incomeOrExpenditure[i].value);
             if(value === "income") {
+                //variable to hold the income list where the new item will be appended to
+                let incomeArea = document.getElementById("incoming");
+
+                //create the h3 to hold the what for value
+                let title = document.createElement("h3")
+                //set the attribute for the element
+                title.setAttribute("id", "title");
+                //get the value of the input
+                let workCompleted = document.getElementById("what-for").value;
+                console.log(workCompleted);
                 console.log("red");
             } else {
                 console.log("blue");
@@ -23,7 +31,7 @@ const createPayment = (ev) => {
 }
 
 let person = document.getElementById("who-from").value;
-let workCompleted = document.getElementById("what-for").value;
+
 
 //each created payment needs to have a reference
 
