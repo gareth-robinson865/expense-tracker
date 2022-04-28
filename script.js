29c162f22ae0e5036852cc5253cb183f7d652021
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-04-28 17:52:44 +0100

    code rearranged so assembly of list item now happens outside of control flow regarding which list the item will be placed on

--- script.js
+++ script.js
@@ -9,45 +9,42 @@ const createPayment = (ev) => {
     for(i=0; i < incomeOrExpenditure.length; i++) {
         if(incomeOrExpenditure[i].checked) {
             let value = incomeOrExpenditure[i].value;
+             
+             //create a new list item to go into the unordered list
+             let newListItem = document.createElement("li");
+
+
+             //create the title element
+             let titleArea = document.createElement("h3")
+             //set the attribute for the element
+             titleArea.setAttribute("id", "title");
+             //get the value of the input
+             let title = document.getElementById("what-for").value;
+             //create text node to be appended to the h3 element;
+             let titleText = document.createTextNode(title);
+             //append text to element
+             titleArea.appendChild(titleText);
+             //append the title to the list item
+             newListItem.appendChild(titleArea);
+             console.log(title);
+
+
+
             if(value === "income") {
-                //variable to hold the income list where the new item will be appended to
+               //variable to hold the income list where the new item will be appended to
                 let incomeArea = document.getElementById("incoming");
-
-                //create a new list item to go into the unordered list
-                let newListItem = document.createElement("li");
-
-
-                //create the title element
-                let titleArea = document.createElement("h3")
-                //set the attribute for the element
-                titleArea.setAttribute("id", "title");
-                //get the value of the input
-                let title = document.getElementById("what-for").value;
-                //create text node to be appended to the h3 element;
-                let titleText = document.createTextNode(title);
-                //append text to element
-                titleArea.appendChild(titleText);
-                //append the title to the list item
-                newListItem.appendChild(titleArea);
-                console.log(title);
-
-
-
-
-
-
-
-
-
                 //append the li element to the unordered list
                 incomeArea.appendChild(newListItem);
                 console.log("red");
             } else {
+                let expenditureArea = document.getElementById("outgoing");
+                expenditureArea.appendChild(newListItem);
                 console.log("blue");
             }
         }
     }
     //need to create control flow here for which radio button is highlighted
+    //everything can happen outside of the control flow in terms of the creation of the element
     
 }
 
